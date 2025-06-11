ba500429f0eaa83291b7eb06cd94da49fd4812a1
DhruvTambekar24 <d.tambekar24@gmail.com>
2025-06-11 11:17:47 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count Inversions/count-inversions.java
+++ Difficulty: Medium/Count Inversions/count-inversions.java
@@ -4,29 +4,49 @@ class Solution {
     }
 
     private long mergeSort(int[] arr, int[] temp, int left, int right) {
-        if (left >= right) return 0;
-        int mid = (left + right) / 2;
-        long inv = 0;
-        inv += mergeSort(arr, temp, left, mid);
-        inv += mergeSort(arr, temp, mid + 1, right);
-        inv += merge(arr, temp, left, mid, right);
-        return inv;
+        long cnt=0;
+        int mid;
+        if(left>=right) return cnt;
+           mid=(left+right)/2;
+           cnt += mergeSort(arr,temp,left,mid);
+           cnt += mergeSort(arr,temp,mid+1,right);
+           cnt += merge(arr,temp,left,mid,right);
+        return cnt;
     }
 
     private long merge(int[] arr, int[] temp, int left, int mid, int right) {
-        int i = left, j = mid + 1, k = left;
-        long inv = 0;
-        while (i <= mid && j <= right) {
-            if (arr[i] <= arr[j]) temp[k++] = arr[i++];
-            else {
-                temp[k++] = arr[j++];
-                inv += (mid - i + 1);
+        int i=left;
+        int j= mid+1;
+        int k=left;
+        long count=0;
+        while(i<=mid && j<=right){
+            if(arr[i]<=arr[j]){
+                temp[k]=arr[i];
+                k++;
+                i++;
             }
+            else{
+                count=count+(mid-i +1);
+                temp[k]=arr[j];
+                k++;
+                j++;
+            }
+            
         }
-        while (i <= mid) temp[k++] = arr[i++];
-        while (j <= right) temp[k++] = arr[j++];
-        System.arraycopy(temp, left, arr, left, right - left + 1);
-        return inv;
+        while(i<=mid){
+            temp[k]=arr[i];
+            i++;
+            k++;
+        }
+        while(j<=right){
+            temp[k]=arr[j];
+            j++;
+            k++;
+        }
+        for(int m =left; m <=right; m++){
+           arr[m]=temp[m];
+        }
+        return count;
     }
 }
 
